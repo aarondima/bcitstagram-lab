a338191c8ac6f742e5bfff06a415eee6c8b4f6f2
aarondima <aarondima@gmail.com>
2024-02-15 11:12:23 -0800

    add starter code

---
+++ IOhandler.js
@@ -0,0 +1,62 @@
+/*
+ * Project: Milestone 1
+ * File Name: IOhandler.js
+ * Description: Collection of functions for files input/output related operations
+ *
+ * Created Date:
+ * Author:
+ *
+ */
+
+const { pipeline } = require("stream");
+
+const yauzl = require("yauzl-promise"),
+  fs = require("fs"),
+  PNG = require("pngjs").PNG,
+  path = require("path");
+
+/**
+ * Description: decompress file from given pathIn, write to given pathOut
+ *
+ * @param {string} pathIn
+ * @param {string} pathOut
+ * @return {promise}
+ */
+async function unzip (pathIn, pathOut) {
+  const zip = await yauzl.open(pathIn)
+  const readStream = fs.createReadStream(zip);
+  const writeStream = fs.createWriteStream(pathOut);
+  pipeline(
+    readStream, 
+    writeStream
+  )
+  
+};
+
+/**
+ * Description: read all the png files from given directory and return Promise containing array of each png file path
+ *
+ * @param {string} path
+ * @return {promise}
+ */
+const readDir = (dir) => {
+
+};
+
+/**
+ * Description: Read in png file by given pathIn,
+ * convert to grayscale and write to given pathOut
+ *
+ * @param {string} filePath
+ * @param {string} pathProcessed
+ * @return {promise}
+ */
+const grayScale = (pathIn, pathOut) => {};
+
+const sepiaFilter = () => {};
+
+module.exports = {
+  unzip,
+  readDir,
+  grayScale,
+};
